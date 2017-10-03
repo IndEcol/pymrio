3f61707a3ce6bb0dd6a19bae8355fd1c16a09ede
Konstantin Stadler <konstantin.stadler@ntnu.no>
2017-10-03 14:24:21 +0200

    updated conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,6 +23,7 @@ import os
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration -----------------------------------------------------
 
