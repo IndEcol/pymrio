3bb95c8529cee3e8ecf26b27da274e6ae32dbc68
Konstantin Stadler <konstantin.stadler@ntnu.no>
2017-10-03 14:29:58 +0200

    updated conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,6 +24,7 @@ import os
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../..'))
 
 # -- General configuration -----------------------------------------------------
 
