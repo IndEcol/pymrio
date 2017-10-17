37dc0ef62c3488ae25653944f722181021829efa
Konstantin Stadler <konstantin.stadler@ntnu.no>
2017-10-17 16:43:31 +0200

    added mock for tkinter in conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,7 @@ import os
 
 # add the pymrio path for the autodoc function
 
+autodoc_mock_imports = ['_tkinter']
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
