154828f6ffcde14d27dd68475a587a14507ad90a
Konstantin Stadler <konstantin.stadler@ntnu.no>
2017-10-17 16:48:22 +0200

    remove mock again

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,8 +15,6 @@ import os
 
 # add the pymrio path for the autodoc function
 
-autodoc_mock_imports = ['_tkinter']
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
