e657f33d075cca9277844d80b6afe2ccf562582a
Konstantin Stadler <konstantin.stadler@ntnu.no>
2019-07-29 16:03:29 +0200

    update to 3.7

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
                      ],
     classifiers=[
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3 :: Only',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Development Status :: 4 - Beta',
