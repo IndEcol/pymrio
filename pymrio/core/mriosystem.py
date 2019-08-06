2f5bbf8176ec386dac7e569e30d90649582927cb
Konstantin Stadler <konstantin.stadler@ntnu.no>
2019-08-06 12:58:12 +0200

    Added equality comparison for IOSystem and Extension

--- pymrio/core/mriosystem.py
+++ pymrio/core/mriosystem.py
@@ -80,6 +80,19 @@ class CoreSystem():
                              '__' not in attr])
         return startstr + parastr
 
+    def __eq__(self, other):
+        """ Only the dataframes are compared. """
+        for key, item in self.__dict__.items():
+            if type(item) is pd.DataFrame:
+                if key not in other.__dict__:
+                    break
+                if not item.equals(other.__dict__[key]):
+                    break
+        else:
+            return True
+
+        return False
+
     def reset_full(self, force=False, _meta=None):
         """ Remove all accounts which can be recalculated based on Z, Y, F, FY
 
@@ -1426,6 +1439,19 @@ class IOSystem(CoreSystem):
 
     def __str__(self):
         return super().__str__("IO System with parameters: ")
+
+    def __eq__(self, other):
+        """ Only the dataframes are compared. """
+        self_ext = set(self.get_extensions(data=False))
+        other_ext = set(other.get_extensions(data=False))
+        if len(self_ext.difference(other_ext)) < 0:
+            return False
+
+        for ext in self_ext:
+            if self.__dict__[ext] != other.__dict__[ext]:
+                return False
+
+        return super().__eq__(other)
 
     @property
     def name(self):
