37c6b280a4ddc07452cf1c404bca6e0b6adbe56b
Konstantin Stadler <konstantin.stadler@ntnu.no>
2020-11-12 13:31:11 +0100

    added explicit python versions in setup.py (these are tested in .travis)

--- setup.py
+++ setup.py
@@ -28,6 +28,8 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Development Status :: 4 - Beta",
